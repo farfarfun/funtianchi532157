79e92280669d6fca2a3c8767c8911119759f349b
niuliangtao <niuliangtao@qq.com>
2023-10-17 19:21:02 +0800

    update

---
+++ funtianchi/init.sh
@@ -0,0 +1,2 @@
+conda create -n dj_comp python=3.10
+conda activate dj_comp
--- funtianchi/install.sh
+++ funtianchi/install.sh
@@ -1,16 +1,16 @@
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple funget funfile
+pip install -i http://mirrors.aliyun.com/pypi/simple/ funget funfile
 
 #pip install git+https://github.com/HYLcool/simhash-py.git
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple git+https://gitee.com/funtianchi/simhash-py.git
+pip install -i http://mirrors.aliyun.com/pypi/simple/ git+https://gitee.com/funtianchi/simhash-py.git
 
 # for training
 cd lm-training
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+pip install -i http://mirrors.aliyun.com/pypi/simple/ -r requirements.txt
 cd ..
 
 # for evaluation
 cd lm-evaluation-harness
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -e .
+pip install -i http://mirrors.aliyun.com/pypi/simple/ -e .
 cd ..
 
 
@@ -18,5 +18,5 @@ cd ..
 #git clone https://github.com/alibaba/data-juicer
 git clone https://gitee.com/funtianchi/data-juicer
 cd data-juicer
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -v -e .[all]
+pip install -i http://mirrors.aliyun.com/pypi/simple/ -v -e .[all]
 
