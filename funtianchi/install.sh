3889e38dc4a1f55be3de5998f5c4727a4396704d
niuliangtao <niuliangtao@qq.com>
2023-10-18 14:22:36 +0800

    update

--- funtianchi/install.sh
+++ funtianchi/install.sh
@@ -1,20 +1,21 @@
-pip install -U -i http://mirrors.aliyun.com/pypi/simple/ funpypi funbuild
-pip install -U -i http://mirrors.aliyun.com/pypi/simple/ funget funfile
+pip install  -r requirements.txt
+
+pip install funpypi funbuild funget funfile
 yes | pip uninstall funfile && pip install -U git+https://gitee.com/farfarfun/funfile.git
 yes | pip uninstall funget && pip install -U git+https://gitee.com/farfarfun/funget.git
 
 
 #pip install git+https://github.com/HYLcool/simhash-py.git
-pip install -i http://mirrors.aliyun.com/pypi/simple/ git+https://gitee.com/funtianchi/simhash-py.git
+pip install  git+https://gitee.com/funtianchi/simhash-py.git
 
 # for training
 cd lm-training
-pip install -i http://mirrors.aliyun.com/pypi/simple/ -r requirements.txt
+pip install  -r requirements.txt
 cd ..
 
 # for evaluation
 cd lm-evaluation-harness
-pip install -i http://mirrors.aliyun.com/pypi/simple/ -e .
+pip install  -e .
 cd ..
 
 
@@ -22,5 +23,5 @@ cd ..
 #git clone https://github.com/alibaba/data-juicer
 git clone https://gitee.com/funtianchi/data-juicer
 cd data-juicer
-pip install -i http://mirrors.aliyun.com/pypi/simple/ -v -e .[all]
+pip install  -v -e .[all]
 
