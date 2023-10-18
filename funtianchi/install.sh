8c05987d45da3936c40617fc0cd540366fb7cb55
niuliangtao <niuliangtao@qq.com>
2023-10-18 16:26:36 +0800

    update

--- funtianchi/install.sh
+++ funtianchi/install.sh
@@ -1,27 +1,28 @@
 pip install  -r requirements.txt
 
-pip install funpypi funbuild funget funfile
-yes | pip uninstall funfile && pip install -U git+https://gitee.com/farfarfun/funfile.git
-yes | pip uninstall funget && pip install -U git+https://gitee.com/farfarfun/funget.git
+pip install funpypi funbuild funget funfile -i https://pypi.tuna.tsinghua.edu.cn/simple
+yes | pip uninstall funfile && pip install -U git+https://gitee.com/farfarfun/funfile.git -i https://pypi.tuna.tsinghua.edu.cn/simple
+yes | pip uninstall funget && pip install -U git+https://gitee.com/farfarfun/funget.git -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 
 #pip install git+https://github.com/HYLcool/simhash-py.git
-pip install  git+https://gitee.com/funtianchi/simhash-py.git
+pip install  git+https://gitee.com/funtianchi/simhash-py.git -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # for training
 cd lm-training
-pip install  -r requirements.txt
+pip install  -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 cd ..
 
 # for evaluation
 cd lm-evaluation-harness
-pip install  -e .
+pip install  -e . -i https://pypi.tuna.tsinghua.edu.cn/simple
 cd ..
 
 
 # for data-juicer
 #git clone https://github.com/alibaba/data-juicer
-git clone https://gitee.com/funtianchi/data-juicer
-cd data-juicer
-pip install  -v -e .[all]
+pip install git+https://github.com/farfarfun/data-juicer.git -i https://pypi.tuna.tsinghua.edu.cn/simple
+# git clone https://gitee.com/funtianchi/data-juicer
+# cd data-juicer
+# pip install  -v -e .[all]
 
