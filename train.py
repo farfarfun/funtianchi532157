77153c7141dd680b4d5394f014428892234e7329
niuliangtao <niuliangtao@qq.com>
2023-10-18 20:57:11 +0800

    update

--- train.py
+++ train.py
@@ -3,13 +3,14 @@ import subprocess
 
 # get data-juicer code directory
 home_path = os.getcwd()
-home_path = '/home/admin'
+#home_path = '/home/admin'
 home_path = os.path.join(home_path, 'funtianchi532157/funtianchi')
 
 print(f"home_path: {home_path}")
 # path to save refined dataset
 trainer_path = os.path.join(home_path, 'lm-training')
-refine_data_path = f'{home_path}/outputs/refined_data/en_refine.jsonl'
+refine_data_en_path = f'{home_path}/outputs/refined_data/en_refine.jsonl'
+refine_data_zh_path = f'{home_path}/outputs/refined_data/zh_refine.jsonl'
 origin_model_path = f'{home_path}/data/models/falcon-rw-1b'
 finetuned_model_path = f'{home_path}/outputs/finetuned_model'
 eval_path = f'{home_path}/lm-evaluation-harness'
@@ -22,10 +23,20 @@ eval_result_path = f'{home_path}/outputs/eval_results'
 def step31():
     refine_data_cmd = [
         'python',
-        f'{home_path}/process/tools/process_data.py',
+        f'{home_path}/process/process_data.py',
         '--config', f'{home_path}/configs/alpaca_cot/alpaca-cot-en-refine.yaml',
-        '--dataset_path', '/srv/data/raw_data.jsonl',
-        '--export_path', refine_data_path,
+        '--dataset_path', f'{home_path}/data/raw_data/raw_data_en.jsonl',
+        '--export_path', refine_data_en_path,
+        '--limit','20000',
+        '--np', '4']
+    subprocess.run(refine_data_cmd)
+    refine_data_cmd = [
+        'python',
+        f'{home_path}/process/process_data.py',
+        '--config', f'{home_path}/configs/alpaca_cot/alpaca-cot-en-refine.yaml',
+        '--dataset_path', f'{home_path}/data/raw_data/raw_data_zh.jsonl',
+        '--export_path', refine_data_zh_path,
+        '--limit','20000',
         '--np', '4']
     subprocess.run(refine_data_cmd)
 
@@ -34,7 +45,7 @@ def step41():
     training_cmd = ['bash',
                     f'{trainer_path}/train_scripts/train.sh',
                     origin_model_path,
-                    refine_data_path,
+                    refine_data_en_path,
                     finetuned_model_path]
     print(f'training_cmd: {" ".join(training_cmd)}')
     subprocess.run(training_cmd)
